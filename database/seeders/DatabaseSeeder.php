1065cc6e34e5ff119f205934b7f3664160908b02
Пользователь <polzovatel@MacBook-Air-Polzovatel.local>
2025-05-01 18:57:07 +0500

    fix DatabaseSeeder, php artisan db:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-//        \App\Models\User::factory(10)->create();
-//        Master::factory(5)->create();
+        \App\Models\User::factory(10)->create();
+        Master::factory(5)->create();
         Request::factory(10)->create();
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
+
