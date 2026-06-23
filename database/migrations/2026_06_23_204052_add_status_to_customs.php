03758a0fd6f75cd44abb48146de9c839ee81bcfe
HeadSplit <eualku2@gmail.com>
2026-06-24 03:17:57 +0500

    fix

--- database/migrations/2026_06_23_204052_add_status_to_customs.php
+++ database/migrations/2026_06_23_204052_add_status_to_customs.php
@@ -27,7 +27,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('customs', function (Blueprint $table) {
-            //
+            $table->dropColumn('status');
         });
     }
 };
